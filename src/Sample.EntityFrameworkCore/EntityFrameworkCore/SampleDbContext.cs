bb189ef608def62da57da2f515c000f476c7ef70
STARGROUP\st973707 <st973707@mail.hamastar.com.tw>
2024-03-14 16:12:39 +0800

    [feat][DbContext新增UserRoles]

--- src/Sample.EntityFrameworkCore/EntityFrameworkCore/SampleDbContext.cs
+++ src/Sample.EntityFrameworkCore/EntityFrameworkCore/SampleDbContext.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.DataProtection.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
 using Volo.Abp.AuditLogging.EntityFrameworkCore;
 using Volo.Abp.BackgroundJobs.EntityFrameworkCore;
 using Volo.Abp.Data;
@@ -23,6 +25,8 @@ public class SampleDbContext :
     IIdentityDbContext,
     ITenantManagementDbContext
 {
+    // 避免多台主機連接第三方登入發生Exception
+    public DbSet<DataProtectionKey> DataProtectionKeys { get; set; }
     /* Add DbSet properties for your Aggregate Roots / Entities here. */
 
     #region Entities from the modules
@@ -38,20 +42,59 @@ public class SampleDbContext :
      * uses this DbContext on runtime. Otherwise, it will use its own DbContext class.
      */
 
-    //Identity
+    // ========= 內建 Identity =========
+    /// <summary>
+    /// 使用者(帳號)
+    /// </summary>
     public DbSet<IdentityUser> Users { get; set; }
+
+    /// <summary>
+    /// 角色
+    /// </summary>
     public DbSet<IdentityRole> Roles { get; set; }
+
+    /// <summary>
+    /// 使用者角色關聯表
+    /// </summary>
+    public DbSet<IdentityUserRole> UserRole { get; set; }
+
+    /// <summary>
+    /// 
+    /// </summary>
     public DbSet<IdentityClaimType> ClaimTypes { get; set; }
+
+    /// <summary>
+    /// 組織
+    /// </summary>
     public DbSet<OrganizationUnit> OrganizationUnits { get; set; }
+
+    /// <summary>
+    /// 登入紀錄表
+    /// </summary>
     public DbSet<IdentitySecurityLog> SecurityLogs { get; set; }
+
     public DbSet<IdentityLinkUser> LinkUsers { get; set; }
+
     public DbSet<IdentityUserDelegation> UserDelegations { get; set; }
 
-    // Tenant Management
+    // ========= Tenant Management =========
     public DbSet<Tenant> Tenants { get; set; }
+
     public DbSet<TenantConnectionString> TenantConnectionStrings { get; set; }
 
-    #endregion
+    #endregion Entities from the modules
+
+    // ========= 自訂資料表 Custom Tables =========
+
+    #region 範例
+
+    /// <summary>
+    /// 範例實體
+    /// </summary>
+    //public DbSet<SampleEntity> SampleEntity { get; set; }
+
+    #endregion 範例
+
 
     public SampleDbContext(DbContextOptions<SampleDbContext> options)
         : base(options)
@@ -59,6 +102,10 @@ public class SampleDbContext :
 
     }
 
+    /// <summary>
+    /// Migration 建表用
+    /// </summary>
+    /// <param name=
     protected override void OnModelCreating(ModelBuilder builder)
     {
         base.OnModelCreating(builder);
@@ -82,5 +129,23 @@ public class SampleDbContext :
         //    b.ConfigureByConvention(); //auto configure for the base class props
         //    //...
         //});
+
+        // ========= 自訂資料表 Custom Tables =========
+
+
+        #region 範例
+
+        // === 範例 ===
+        //builder.Entity<SampleEntity>(b =>
+        //{
+        //    b.ToTable("SampleEntity");
+
+        //    b.ConfigureByConvention();
+
+        //    b.Property(x => x.Id)
+        //    .IsRequired();
+        //});
+
+        #endregion 範例
     }
 }
