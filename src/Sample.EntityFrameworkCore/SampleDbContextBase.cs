e149a4f974259dca6474529b80605caab3c750a7
STARGROUP\st973707 <st973707@mail.hamastar.com.tw>
2024-03-14 11:05:52 +0800

    [feat][新增泛型化資料庫]

---
+++ src/Sample.EntityFrameworkCore/SampleDbContextBase.cs
@@ -0,0 +1,26 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="SampleDbContextBase.cs" company="Hamastar">
+//     Copyright (c) Hamastar. All rights reserved.
+// </copyright>
+// <author>Samuel</author>
+//-----------------------------------------------------------------------
+
+using Sample.EntityFrameworkCore;
+
+using Volo.Abp.Domain.Entities;
+using Volo.Abp.Domain.Repositories.EntityFrameworkCore;
+using Volo.Abp.EntityFrameworkCore;
+
+namespace Sample.Repositories;
+
+/// <summary>
+/// 泛型化資料庫
+/// </summary>
+public class TFTSDbContextBase<TEntity, TPrimaryKey> : EfCoreRepository<SampleDbContext, TEntity, TPrimaryKey>
+    where TEntity : class, IEntity<TPrimaryKey>
+{
+    protected TFTSDbContextBase(IDbContextProvider<SampleDbContext> dbContextProvider)
+            : base(dbContextProvider)
+    {
+    }
+}
