9aa75f0419efa79c82055d8a9b48b2b022402aea
STARGROUP\st973707 <st973707@mail.hamastar.com.tw>
2024-03-15 17:34:42 +0800

    [feat][增加權限]

--- src/Sample.Application.Contracts/Permissions/SamplePermissionDefinitionProvider.cs
+++ src/Sample.Application.Contracts/Permissions/SamplePermissionDefinitionProvider.cs
@@ -1,16 +1,49 @@
-﻿using Sample.Localization;
+﻿using Microsoft.Extensions.Localization;
+
+using Sample.Localization;
 using Volo.Abp.Authorization.Permissions;
 using Volo.Abp.Localization;
 
 namespace Sample.Permissions;
 
+/// <summary>
+/// 權限：加入群組
+/// </summary>
 public class SamplePermissionDefinitionProvider : PermissionDefinitionProvider
 {
+    #region Fields
+
+    private readonly IStringLocalizer<SampleResource> localizer;
+
+    #endregion Fields
+
+    #region Constructor
+
+    public SamplePermissionDefinitionProvider(IStringLocalizer<SampleResource> localizer)
+    {
+        this.localizer = localizer;
+    }
+
+    #endregion Constructor
+
+    /// <summary>
+    /// 定義所有權限
+    /// </summary>
+    /// <param name="context"> IPermissionDefinitionContext </param>
     public override void Define(IPermissionDefinitionContext context)
     {
-        var myGroup = context.AddGroup(SamplePermissions.GroupName);
+        var sampleGroup = context.AddGroup(SamplePermissions.GroupName);
         //Define your own permissions here. Example:
         //myGroup.AddPermission(SamplePermissions.MyPermission1, L("Permission:MyPermission1"));
+
+        // 【內容管理功能】
+        // ========= 系統管理 =========
+        // 系統管理
+        var systemManagementPermission = sampleGroup.AddPermission(SamplePermissions.SystemManagement.Default, L("Permission:SystemManagement"));
+        systemManagementPermission.AddChild(SamplePermissions.SystemManagement.Create, L("Permission:SystemManagement.Create"));
+        systemManagementPermission.AddChild(SamplePermissions.SystemManagement.Edit, L("Permission:SystemManagement.Edit"));
+        systemManagementPermission.AddChild(SamplePermissions.SystemManagement.Delete, L("Permission:SystemManagement.Delete"));
+
     }
 
     private static LocalizableString L(string name)
--- src/Sample.Application.Contracts/Permissions/SamplePermissions.cs
+++ src/Sample.Application.Contracts/Permissions/SamplePermissions.cs
@@ -1,9 +1,28 @@
 ﻿namespace Sample.Permissions;
 
+/// <summary>
+/// 權限：定義欄位值、層級列表
+/// </summary>
 public static class SamplePermissions
 {
+    #region Fields 定義權限Key值
     public const string GroupName = "Sample";
 
     //Add your own permission names. Example:
     //public const string MyPermission1 = GroupName + ".MyPermission1";
+
+    // 【內容管理功能】
+    // ========= 系統管理 =========
+    /// <summary>
+    /// 系統管理 權限
+    /// </summary>
+    public static class SystemManagement
+    {
+        public const string Default = GroupName + ".SystemManagement";
+        public const string Create = Default + ".Create";
+        public const string Edit = Default + ".Edit";
+        public const string Delete = Default + ".Delete";
+    }
+
+    #endregion Fields 定義權限Key值
 }
